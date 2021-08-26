ec42905591f8df96fcede174060f2abb8d4b070f
wakachico <86215584+wakachico@users.noreply.github.com>
2021-08-26 23:08:01 +0900

    rubocop修正

--- spec/requests/ideas_spec.rb
+++ spec/requests/ideas_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Ideas", type: :request do
+RSpec.describe 'Ideas', type: :request do
   before do
     @idea = FactoryBot.create(:idea)
     @another_idea = FactoryBot.create(:idea)
@@ -8,63 +8,63 @@ RSpec.describe "Ideas", type: :request do
   end
 
   describe 'POST #create 1.アイデア登録API' do
-    it 'リクエストのcategory_nameがcategoriesテーブルのnameに存在する場合、categoryのidをcategory_idとして、ideasテーブルに登録される' do 
-      valid_params = { category_name: @idea.category.name , body: 'test'}
+    it 'リクエストのcategory_nameがcategoriesテーブルのnameに存在する場合、categoryのidをcategory_idとして、ideasテーブルに登録される' do
+      valid_params = { category_name: @idea.category.name, body: 'test' }
       # categorysテーブルにはデータが作成されない事を確認
-      expect{ 
+      expect do
         post '/ideas', params: valid_params
-      }.to change(Category, :count).by(0)
+      end.to change(Category, :count).by(0)
       # ideasテーブルにデータが作成されている事を確認
-      expect{ 
+      expect do
         post '/ideas', params: valid_params
-      }.to change(Idea, :count).by(+1)
+      end.to change(Idea, :count).by(+1)
       # HTTPステータスコード201が返却される
       expect(response.status).to eq 201
     end
-    it 'リクエストのcategory_nameがcategoriesテーブルのnameに存在しない場合、新たなcategoryとしてcategoriesテーブルに登録し、ideasテーブルに登録される' do 
-      valid_params = { category_name: 'ダミーカテゴリー' , body: 'test'}
+    it 'リクエストのcategory_nameがcategoriesテーブルのnameに存在しない場合、新たなcategoryとしてcategoriesテーブルに登録し、ideasテーブルに登録される' do
+      valid_params = { category_name: 'ダミーカテゴリー', body: 'test' }
       # categorysテーブルにデータが作成されている事を確認
-      expect{ 
+      expect do
         post '/ideas', params: valid_params
-      }.to change(Category, :count).by(+1)
+      end.to change(Category, :count).by(+1)
       # ideasテーブルにデータが作成されている事を確認
-      expect{ 
+      expect do
         post '/ideas', params: valid_params
-      }.to change(Idea, :count).by(+1)
+      end.to change(Idea, :count).by(+1)
       # HTTPステータスコード201が返却される
       expect(response.status).to eq 201
     end
-    it 'リクエストのcategory_nameが空の場合、ステータスコード422が返却される' do 
-      valid_params = { category_name: '' , body: 'test'}
+    it 'リクエストのcategory_nameが空の場合、ステータスコード422が返却される' do
+      valid_params = { category_name: '', body: 'test' }
       # categorysテーブルにデータが作成されない事を確認
-      expect{ 
+      expect do
         post '/ideas', params: valid_params
-      }.to change(Category, :count).by(0)
+      end.to change(Category, :count).by(0)
       # ideasテーブルにデータが作成されない事を確認
-      expect{ 
+      expect do
         post '/ideas', params: valid_params
-      }.to change(Idea, :count).by(0)
+      end.to change(Idea, :count).by(0)
       # HTTPステータスコード422が返却される
       expect(response.status).to eq 422
     end
-    it 'リクエストのbodyが空の場合、ステータスコード422が返却される' do 
-      valid_params = { category_name: @idea.category.name , body: ''}
+    it 'リクエストのbodyが空の場合、ステータスコード422が返却される' do
+      valid_params = { category_name: @idea.category.name, body: '' }
       # categorysテーブルにデータが作成されない事を確認
-      expect{ 
+      expect do
         post '/ideas', params: valid_params
-      }.to change(Category, :count).by(0)
+      end.to change(Category, :count).by(0)
       # ideasテーブルにデータが作成されない事を確認
-      expect{ 
+      expect do
         post '/ideas', params: valid_params
-      }.to change(Idea, :count).by(0)
+      end.to change(Idea, :count).by(0)
       # HTTPステータスコード422が返却される
       expect(response.status).to eq 422
     end
   end
 
   describe 'GET #index 2.アイデア取得API' do
-    it 'indexアクションにcategory_nameをリクエストで指定しないとと全てのideasが返却される' do 
-      dummy_ideas = FactoryBot.create_list(:idea, 5)
+    it 'indexアクションにcategory_nameをリクエストで指定しないとと全てのideasが返却される' do
+      FactoryBot.create_list(:idea, 5)
       valid_params = { category_name: '' }
       get ideas_path, params: valid_params
       json = JSON.parse(response.body)
@@ -73,8 +73,8 @@ RSpec.describe "Ideas", type: :request do
       # 全てのideasの一覧が返却される(@idea,another_idea,5件のダミーデータ計：7件)
       expect(json['data'].length).to eq(7)
     end
-    it 'indexアクションに既に保存されているcategory_nameをリクエストで指定すると該当するcategoryのideasの一覧が返却される' do 
-      dummy_ideas = FactoryBot.create_list(:idea, 5)
+    it 'indexアクションに既に保存されているcategory_nameをリクエストで指定すると該当するcategoryのideasの一覧が返却される' do
+      FactoryBot.create_list(:idea, 5)
       valid_params = { category_name: @idea.category.name }
       get ideas_path, params: valid_params
       json = JSON.parse(response.body)
@@ -87,7 +87,7 @@ RSpec.describe "Ideas", type: :request do
         expect(json['data'][i]['category']).to eq(@idea.category.name)
       end
     end
-    it 'indexアクションに登録されていないカテゴリーのリクエストで指定するとステータスコード404が返却される' do 
+    it 'indexアクションに登録されていないカテゴリーのリクエストで指定するとステータスコード404が返却される' do
       dummy_params = 'ダミーデータ'
       valid_params = { category_name: dummy_params }
       get ideas_path, params: valid_params
